30af41cfd7da1a324b617dc937cb6cbee831959e
John Young <jayelwhy@yahoo.com>
2015-04-24 14:03:37 -0700

    code cleaned up

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -1,7 +1,10 @@
 class String
   define_method(:word_freq) do |string|
     words = string.split(" ")
-
-
+    counter = Hash.new()
+    counter.merge!(key: self)
+    words.each do |y|
+    counter["y"] +=1
   end
+end
 end
